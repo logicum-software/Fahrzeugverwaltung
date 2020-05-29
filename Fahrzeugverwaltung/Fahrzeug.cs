e26357256a16370d4811113e44a7f7df2776c257
Markus Gärtner <gaertner.brilon@gmail.com>
2020-05-29 07:50:18 +0200

    Projektdateien hinzufügen.

---
+++ Fahrzeugverwaltung/Fahrzeug.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fahrzeugverwaltung
+{
+    [Serializable]
+    class Fahrzeug
+    {
+        private String strKennzeichen;
+        private String strEinrichtung;
+        private String strFahrzeugtyp;
+        private int iMandant;
+        private int iKostenstelle;
+        private DateTime dtErstzulassung;
+        private String strWartungsfirma;
+        private String strBesonderheiten;
+        private String strVerantwortung;
+        private String strStandort;
+        private DateTime dtAblaufdatum;
+        private DateTime dtNeubeschaffung;
+        private int iLeasinglaufzeit;
+        private int iLeasinglaufleistung;
+    }
+}
